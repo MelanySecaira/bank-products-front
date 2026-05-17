30da65b3e85afaca66691d4dc191cb2e946103dc
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2026-05-17 17:25:50 -0500

    feat: initialize products feature architecture

---
+++ client/src/app/core/services/product.ts
@@ -0,0 +1,87 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
+import { Product } from '../../features/products/models/product.model';
+import { ApiResponse } from '../models/api-response.model';
+import { ProductUpdateRequest } from '../../features/products/models/product-update-request.model';
+import { ProductCreateRequest } from '../../features/products/models/product-create-request.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ProductService {
+
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}/bp/products`;
+
+  getProducts(): Observable<Product[]> {
+
+    return this.http
+      .get<ApiResponse<Product[]>>(this.apiUrl)
+      .pipe(
+        map(response => response.data)
+      );
+  }
+
+  getProductById(id: string): Observable<Product> {
+
+    return this.http
+      .get<ApiResponse<Product>>(
+        `${this.apiUrl}/${id}`
+      )
+      .pipe(
+        map(response => response.data)
+      );
+  }
+
+  createProduct(
+    product: ProductCreateRequest
+  ): Observable<Product> {
+
+    return this.http
+      .post<ApiResponse<Product>>(
+        this.apiUrl,
+        product
+      )
+      .pipe(
+        map(response => response.data)
+      );
+  }
+
+  updateProduct(
+    id: string,
+    product: ProductUpdateRequest
+  ): Observable<Product> {
+
+    return this.http
+      .put<ApiResponse<Product>>(
+        `${this.apiUrl}/${id}`,
+        product
+      )
+      .pipe(
+        map(response => response.data)
+      );
+  }
+
+  
+  verifyId(id: string): Observable<boolean> {
+
+    return this.http.get<boolean>(
+      `${this.apiUrl}/verification/${id}`
+    );
+  }
+
+  deleteProduct(id: string) {
+
+    return this.http.delete<ApiResponse<void>>(
+      `${this.apiUrl}/${id}`
+    );
+  }
+
+
+}
---
+++ client/src/app/features/products/models/product-update-request.model.ts
@@ -0,0 +1,7 @@
+export interface ProductUpdateRequest {
+  name: string;
+  description: string;
+  logo: string;
+  date_release: string;
+  date_revision: string;
+}
